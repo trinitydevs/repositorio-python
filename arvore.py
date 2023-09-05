1324a53711994d48a17adc351e993c930b478d8a
Trinity <111662820+TrinityDN@users.noreply.github.com>
2023-09-05 15:52:18 -0300

    arvore em python

---
+++ arvore.py
@@ -0,0 +1,18 @@
+#Árvore de natal em asteriscos
+asterisco = "*"
+espaco = " "
+print(espaco*8, asterisco)
+print(espaco*7, asterisco*3)
+print(espaco*6,asterisco*5)
+print(espaco*5,asterisco*7)
+print(espaco*4,asterisco*9)
+print(espaco*3,asterisco*11)
+print(espaco*2,asterisco*13)
+print(espaco*1,asterisco*15)
+print(espaco*0, asterisco*17)
+print(asterisco*19)
+print(espaco*7, asterisco*3)
+print(espaco*7, asterisco*3)
+print(espaco*7, asterisco*3)
+
+
